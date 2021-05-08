2c58862b8974f92928decad1e7a7ec5506091dea
Bertuğ <73167951+bertuginal@users.noreply.github.com>
2021-05-08 18:57:48 +0300

    Add files via upload

---
+++ KDVli_Urun_Fiyatlari_Projesi/KDVli_Urun_Fiyatlari_Projesi/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Sinav_Notlari_Hesaplama_Uygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(66, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 43);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Göster";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 29);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Ürün Adı :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 29);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Ürün Fiyatı :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(157, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 29);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "---";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Muz",
+            "Çikolata",
+            "Bilgisayar"});
+            this.comboBox1.Location = new System.Drawing.Point(157, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 36);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(326, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(372, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.ItemHeight = 28;
+            this.listBox1.Location = new System.Drawing.Point(12, 243);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(686, 256);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(710, 511);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ KDVli_Urun_Fiyatlari_Projesi/KDVli_Urun_Fiyatlari_Projesi/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinav_Notlari_Hesaplama_Uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String urunadi;
+            int urunfiyati, kdv, yenifiyat;
+            urunadi = comboBox1.Text;
+
+            if (urunadi == "Muz")
+            {
+                urunfiyati = 20;
+                label8.Text = urunfiyati.ToString();
+                urunfiyati = Convert.ToInt16(label8.Text);
+                kdv = (urunfiyati * 50) / 100;
+                yenifiyat = urunfiyati + kdv;
+
+                listBox1.Items.Add("Ürünün adı : " + urunadi + "   Ürünün Fiyatı : " + urunfiyati + "   %50 KDV'li Fiyatı : " + yenifiyat);
+            }
+            else if (urunadi == "Çikolata")
+            {
+                urunfiyati = 5;
+                label8.Text = urunfiyati.ToString();
+                urunfiyati = Convert.ToInt16(label8.Text);
+                kdv = (urunfiyati * 50) / 100;
+                yenifiyat = urunfiyati + kdv;
+
+                listBox1.Items.Add("Ürünün adı : " + urunadi + "   Ürünün Fiyatı : " + urunfiyati + "   %50 KDV'li Fiyatı : " + yenifiyat);
+
+            }
+            else if (urunadi == "Bilgisayar")
+            {
+                urunfiyati = 10000;
+                label8.Text = urunfiyati.ToString();
+                urunfiyati = Convert.ToInt16(label8.Text);
+                kdv = (urunfiyati * 50) / 100;
+                yenifiyat = urunfiyati + kdv;
+
+                listBox1.Items.Add("Ürünün adı : " + urunadi + "   Ürünün Fiyatı : " + urunfiyati + "   %50 KDV'li Fiyatı : " + yenifiyat);
+
+            }
+
+
+
+        }
+    }
+}
